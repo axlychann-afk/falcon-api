70700c63c2dfb3ae28553d841bf6bbce83101cd0
axlychann-afk <axlychann@gmail.com>
2026-05-19 01:58:51 +0700

    Refactor Google Image search to use axios and cheerio

--- src/api/search/googleimage.js
+++ src/api/search/googleimage.js
@@ -1,16 +1,48 @@
-var gis = require('async-g-i-s');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-module.exports = function (app) {
-app.get('/search/gimage', async (req, res) => {
-       const { q } = req.query
+module.exports = (app) => {
+    app.get('/search/gimage', async (req, res) => {
+        const { q } = req.query;
+
+        if (!q) {
+            return res.status(400).json({
+                status: false,
+                error: 'Parameter "q" diperlukan'
+            });
+        }
+
         try {
-            const results = await gis(q);  
-            res.status(200).json({
+            const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(q)}&tbm=isch`;
+            const response = await axios.get(searchUrl, {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                }
+            });
+
+            const $ = cheerio.load(response.data);
+            const images = [];
+
+            $('img').each((i, el) => {
+                const src = $(el).attr('src');
+                if (src && src.startsWith('http') && images.length < 20) {
+                    images.push(src);
+                }
+            });
+
+            res.json({
                 status: true,
-                result: results
+                creator: 'FlowFalcon',
+                result: {
+                    query: q,
+                    images: images
+                }
             });
         } catch (error) {
-            res.status(500).send(`Error: ${error.message}`);
+            res.status(500).json({
+                status: false,
+                error: error.message
+            });
         }
-});
-}
+    });
+};
