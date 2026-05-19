3b9443b28efff8c5f19ccea6c4158f7924c4a69c
axlychann-afk <axlychann@gmail.com>
2026-05-19 19:14:45 +0700

    Update Jadwaljawa.js

--- src/api/anime/Jadwaljawa.js
+++ src/api/anime/Jadwaljawa.js
@@ -50,7 +50,7 @@ module.exports = function (app) {
     if (result?.error) {
       return res.status(500).json({
         status: false,
-        creator: 'FlowFalcon',
+        creator: 'AxlyChann',
         message: 'Gagal mengambil jadwal rilis anime',
         error: result.message
       });
