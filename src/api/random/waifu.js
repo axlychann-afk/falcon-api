8f17fa252aaf2c5f29cb8ebfe8e30597e145cf02
axlychann-afk <axlychann@gmail.com>
2026-05-23 19:42:55 +0700

    Update waifu.js

--- src/api/random/waifu.js
+++ src/api/random/waifu.js
@@ -3,18 +3,20 @@ const axios = require('axios');
 module.exports = (app) => {
     app.get('/random/waifu', async (req, res) => {
         try {
-            const response = await axios.get('https://api.waifu.im/random/?selected_tags=waifu', {
+            // Pake Nekos.life API (gratis, stabil)
+            const response = await axios.get('https://nekos.life/api/v2/img/waifu', {
                 timeout: 10000
             });
 
-            if (!response.data?.images?.length) {
+            if (!response.data?.url) {
                 throw new Error('Gambar tidak ditemukan');
             }
 
-            const imageUrl = response.data.images[0].url;
+            const imageUrl = response.data.url;
             res.redirect(imageUrl);
 
         } catch (error) {
+            console.error(error);
             res.status(500).json({
                 status: false,
                 error: 'Gagal mengambil gambar waifu'
